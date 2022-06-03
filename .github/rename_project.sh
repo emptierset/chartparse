589e9f88535872122bd545783c8a9d59675922e2
AWConant <AWConant@users.noreply.github.com>
2022-06-03 23:30:32 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="AWConant"
+original_name="chartparse"
+original_urlname="chartparse"
+original_description="Awesome chartparse created by AWConant"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv chartparse $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
