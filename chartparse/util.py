f14335f227f6e3addc8dd627756d9e437153aca0
emptyset <emptierset@gmail.com>
2022-06-26 14:22:46 -0700

    Add ReadOnlyList
    To be amended.

--- chartparse/util.py
+++ chartparse/util.py
@@ -28,3 +28,28 @@ def iterate_from_second_elem(xs):
     it = iter(xs)
     next(it)
     yield from it
+
+
+class ReadOnlyList(list):
+    def __init__(self, other):
+        self._list = other
+
+    def __getitem__(self, index):
+        return self._list[index]
+
+    def __iter__(self):
+        return iter(self._list)
+
+    def __slice__(self, *args, **kw):
+        return self._list.__slice__(*args, **kw)
+
+    def __repr__(self):
+        return repr(self._list)
+
+    def __len__(self):
+        return len(self._list)
+
+    def NotImplemented(self, *args, **kw):
+        raise ValueError("Read Only list proxy")
+
+    append = pop = __setitem__ = __setslice__ = __delitem__ = NotImplemented
