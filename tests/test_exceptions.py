ce25cf8cab8099b6db1b22aad1ae4e34ed630edd
emptyset <emptierset@gmail.com>
2023-01-27 22:30:32 -0500

    test: typecheck test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -9,41 +9,39 @@ from chartparse.exceptions import (
     raise_,
 )
 
-# TODO: typecheck the tests in this file by adding "-> None" annotations to each test function.
-
 
 class TestRaise(object):
-    def test(self):
+    def test(self) -> None:
         with pytest.raises(ValueError):
-            raise_(ValueError)
+            raise_(ValueError())
 
 
 class TestMissingRequiredField(object):
-    def test(self):
+    def test(self) -> None:
         got = MissingRequiredField("foo")
         assert got.field_name == "foo"
         assert got.message == "unable to find a chart line matching required field 'foo'"
 
 
 class TestRegexNotMatchError(object):
-    def test_regex_only(self):
+    def test_regex_only(self) -> None:
         got = RegexNotMatchError(r"\d+")
         assert got.regex == r"\d+"
         assert got.message == r"regex '\d+' failed to match"
 
-    def test_regex_and_s(self, invalid_chart_line):
+    def test_regex_and_s(self, invalid_chart_line) -> None:
         got = RegexNotMatchError(r"\d+", invalid_chart_line)
         assert got.regex == r"\d+"
         assert got.message == rf"string '{invalid_chart_line}' failed to match regex '\d+'"
 
-    def test_regex_and_collection(self, invalid_chart_line):
+    def test_regex_and_collection(self, invalid_chart_line) -> None:
         got = RegexNotMatchError(r"\d+", [invalid_chart_line, invalid_chart_line])
         assert got.regex == r"\d+"
         assert got.message == r"none of 2 strings matched regex '\d+'"
 
 
 class TestUnreachableError(object):
-    def test(self):
+    def test(self) -> None:
         reason = "can't be reached"
         got = UnreachableError(reason)
         assert got.message == reason
