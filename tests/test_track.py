b43bb621012ab35bc28abc0cd796f83947704205
Andrew Conant <emptierset@gmail.com>
2023-02-06 13:44:46 -0500

    test: typecheck test_track.py

--- tests/test_track.py
+++ tests/test_track.py
@@ -1,68 +1,76 @@
 from __future__ import annotations
 
 import dataclasses
+import typing as typ
 
 import chartparse.track
 from chartparse.event import Event
-from chartparse.sync import AnchorEvent, BPMEvent
-from chartparse.tick import Tick
+from chartparse.sync import BPMEvent
+from chartparse.tick import Tick, Ticks
 from chartparse.track import build_events_from_data, parse_data_from_chart_lines
 from tests.helpers import defaults, testcase
 from tests.helpers.fruit import Fruit
 from tests.helpers.log import LogChecker
 from tests.helpers.sync import BPMEventsWithDefaults
 
-# TODO: typecheck the tests in this file by adding "-> None" annotations to each test function.
-
 
 class TestBuildEventsFromData(object):
-    @testcase.parametrize(
-        ["data", "from_data_return_value", "want"],
-        [
-            testcase.new(
-                "with_timestamp_getter",
-                data=defaults.time_signature_event_parsed_data,
-                from_data_return_value=defaults.time_signature_event,
-                want=[defaults.time_signature_event],
-            ),
-            testcase.new(
-                "with_None",
-                data=defaults.anchor_event_parsed_data,
-                from_data_return_value=defaults.anchor_event,
-                want=[defaults.anchor_event],
-            ),
-            testcase.new(
-                "with_resolution",
-                data=defaults.bpm_event_parsed_data,
-                from_data_return_value=defaults.bpm_event,
-                want=BPMEventsWithDefaults(),
-            ),
-        ],
-    )
-    def test(
+    def with_bpm_events_getter(
         self,
         mocker,
         invalid_chart_line,
         minimal_bpm_events_with_mock,
-        data,
-        from_data_return_value,
-        want,
-    ):
-        from_data_fn_mock = mocker.Mock(return_value=from_data_return_value)
-        if isinstance(from_data_return_value, BPMEvent):
-            resolution_or_bpm_events_or_None = defaults.resolution
-        elif isinstance(from_data_return_value, AnchorEvent):
-            resolution_or_bpm_events_or_None = None
-        else:
-            resolution_or_bpm_events_or_None = minimal_bpm_events_with_mock
+    ) -> None:
+        from_data_fn_mock = mocker.Mock(return_value=defaults.time_signature_event)
 
-        got = build_events_from_data([data], from_data_fn_mock, resolution_or_bpm_events_or_None)
+        want = [defaults.time_signature_event]
+        got = build_events_from_data(
+            [defaults.time_signature_event_parsed_data],
+            from_data_fn_mock,
+            minimal_bpm_events_with_mock,
+        )
 
         assert got == want
-        if isinstance(from_data_return_value, AnchorEvent):
-            from_data_fn_mock.assert_called_once_with(data)
-        else:
-            from_data_fn_mock.assert_called_once_with(data, None, resolution_or_bpm_events_or_None)
+        from_data_fn_mock.assert_called_once_with(
+            defaults.time_signature_event_parsed_data, None, minimal_bpm_events_with_mock
+        )
+
+    def test_with_none(
+        self,
+        mocker,
+        invalid_chart_line,
+        minimal_bpm_events_with_mock,
+    ) -> None:
+        from_data_fn_mock = mocker.Mock(return_value=defaults.anchor_event)
+
+        want = [defaults.anchor_event]
+        got = build_events_from_data([defaults.anchor_event_parsed_data], from_data_fn_mock)
+
+        assert got == want
+        from_data_fn_mock.assert_called_once_with(defaults.anchor_event_parsed_data)
+
+    def test_with_resolution(
+        self,
+        mocker,
+        invalid_chart_line,
+        minimal_bpm_events_with_mock,
+    ) -> None:
+        from_data_fn_mock = mocker.Mock(return_value=defaults.bpm_event)
+
+        want = BPMEventsWithDefaults()
+        got = build_events_from_data(
+            [defaults.bpm_event_parsed_data],
+            typ.cast(
+                typ.Callable[[BPMEvent.ParsedData, typ.Optional[BPMEvent], Ticks], BPMEvent],
+                from_data_fn_mock,
+            ),
+            typ.cast(Ticks, defaults.resolution),
+        )
+
+        assert got == want
+        from_data_fn_mock.assert_called_once_with(
+            defaults.bpm_event_parsed_data, None, defaults.resolution
+        )
 
 
 class TestParseDataFromChartLines(object):
@@ -90,11 +98,11 @@ class TestParseDataFromChartLines(object):
             ),
         ],
     )
-    def test(self, types, lines, want):
+    def test(self, types, lines, want) -> None:
         got = parse_data_from_chart_lines(types, lines)
         assert got == want
 
-    def test_no_suitable_parsers(self, mocker, caplog, invalid_chart_line):
+    def test_no_suitable_parsers(self, mocker, caplog, invalid_chart_line) -> None:
         _ = parse_data_from_chart_lines(tuple(), [invalid_chart_line])
         logchecker = LogChecker(caplog)
         logchecker.assert_contains_string_in_one_line(
